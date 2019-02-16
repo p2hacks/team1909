694732535456b85ab5bdfbfd745b6d0148382c29
“Take2pad” <b1017189@fun.ac.jp>
2019-02-16 13:22:39 +0900

    自分のプロフ欄の作成

--- p2hacks/p2hacks/ViewController2.swift
+++ p2hacks/p2hacks/ViewController2.swift
@@ -13,25 +13,44 @@ class ViewController2: UIViewController, UITableViewDelegate, UITableViewDataSou
     var TODO = ["運営"]
     var text = ""
     
+    var userDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        TODO.append(text)
+//        TODO.append(text)
         
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        TODO.append(text)
+        userDefaults.set(TODO, forKey: "TODOlist")
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TODO.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // セルに表示する値を設定する
+        cell.textLabel!.text = TODO[indexPath.row]
+        return cell
+    }
+
     func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
-        // SubViewController へ遷移するために Segue を呼び出す
-        
+        // ProViewController へ遷移するために Segue を呼び出す
         performSegue(withIdentifier: "toProViewController",sender: nil)
     }
     
-    
+//    let getMaxSpeed:[Double] = userDefaults.array(forKey: "udMaxSpeed") as! [Double]
     // Segue 準備
     override func prepare(for segue: UIStoryboardSegue, sender: Any!) {
         if (segue.identifier == "toViewController") {
@@ -44,19 +63,27 @@ class ViewController2: UIViewController, UITableViewDelegate, UITableViewDataSou
             let proViewController = segue.destination as! ProViewController
             proViewController.text = self.text
         }
+        if segue.identifier == "tomyProViewController" {
+            let myproViewController = segue.destination as! myProViewController
+            myproViewController.text = self.text
+        }
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return TODO.count
-    }
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return TODO.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        // セルを取得する
+//        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        // セルに表示する値を設定する
+//        cell.textLabel!.text = TODO[indexPath.row]
+//        return cell
+//    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // セルを取得する
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        // セルに表示する値を設定する
-        cell.textLabel!.text = TODO[indexPath.row]
-        return cell
-    }
+//    override func viewwillAppear(_ animated: Bool) {
+//        TODO = UserDefaults.standard.stringArray(forKey: TODO)
+//    }
     
 }
 
